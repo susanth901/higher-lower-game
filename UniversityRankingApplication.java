83f679f220c44360c38cf1a842788e5d0fcdbc60
susanth901 <p.saisusanth@gmail.com>
2024-09-25 22:12:52 +0530

    Create UniversityRankingApplication.java

---
+++ UniversityRankingApplication.java
@@ -0,0 +1,12 @@
+package com.example.universityranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityRankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityRankingApplication.class, args);
+    }
+}
