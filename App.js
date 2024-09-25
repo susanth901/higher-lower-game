bfaba451fded9a8983f3e6758678780fcdc859dc
susanth901 <p.saisusanth@gmail.com>
2024-09-25 22:09:19 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HigherLowerGame from './HigherLowerGame';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherLowerGame />
+    </div>
+  );
+}
+
+export default App;
